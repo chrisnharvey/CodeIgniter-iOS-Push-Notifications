33e8400897ccecc1bd4699b32dc6068271496b93
Chris Harvey <chris@chrisnharvey.com>
2013-08-13 11:24:23 +0100

    Adding config file which I must have forgot 9 months ago!

---
+++ config/ios.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Live
+$config['host'] = 'gateway.push.apple.com';
+
+// Sandbox
+$config['host'] = 'gateway.sandbox.push.apple.com';
+
+// Certificate
+$config['cert'] = 'PATH_TO_YOUR_CERTIFICATE';
